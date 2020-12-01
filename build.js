13888beb5ceb068322e395f8f74f4920dabaa481
Kyle Landell <kyle.landell@mssm.edu>
2020-12-01 16:47:59 -0500

    Copy images during build

--- build.js
+++ build.js
@@ -5,20 +5,33 @@ async function minifyFile(path) {
   return JSON.stringify(JSON.parse(data));
 }
 
-async function processDirectory(type) {
+async function minifyDirectory(type) {
   const dir = `${__dirname}/${type}`;
   const files = await fs.readdir(dir);
 
   const promises = files.map(async (file) => {
     const path = `${dir}/${file}`;
     const json = await minifyFile(path);
-    await fs.writeFile(path, json);
+    await fs.writeFile(`${__dirname}/dist/${type}`, json);
+  });
+  return Promise.all(promises);
+}
+
+async function copyDirectory(type) {
+  const dir = `${__dirname}/${type}`;
+  const files = await fs.readdir(dir);
+
+  const promises = files.map(async (file) => {
+    const src = `${dir}/${file}`;
+    const dest = `${__dirname}/dist/${type}`;
+    await fs.copyFile(src, dest);
   });
   return Promise.all(promises);
 }
 
 function build() {
-  processDirectory("studies");
-  processDirectory("surveys");
+  minifyDirectory("studies");
+  minifyDirectory("surveys");
+  copyDirectory("images");
 }
 build();
