b3efd8160767a5a8bb41da96e678ef8dc52cf3d6
Kyle Landell <kyle.landell@mssm.edu>
2021-04-27 12:57:52 -0400

    Checkpoint build

--- build.js
+++ build.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
 const YAML = require("yaml");
+
+const validateSchema = require("./validate_schema");
 
 const versions = YAML.parse(
   fs.readFileSync("./version.lock", { encoding: "utf-8" })
 );
+
+const surveyLibrary = new Set();
+
+const diffs = new Map();
+diffs.set("studies", new Map());
+diffs.set("surveys", new Map());
 
 async function mkdir(path) {
   try {
@@ -14,11 +24,89 @@ async function mkdir(path) {
 }
 
 async function processStudies() {
+  const distDir = `${__dirname}/dist/`;
   const studies = await fs.promises.readdir(`${__dirname}/studies`);
 
+  studies.forEach(async (studyKey) => {
+    const studyDir = `${__dirname}/studies/${studyKey}`;
+
+    const studyConfig = YAML.parse(
+      await fs.promises.readFile(`${studyDir}/study.yaml`, {
+        encoding: "utf-8",
+      })
+    );
+
+    // process images
+    const images = await fs.promises.readdir(`${studyDir}/images`);
+    images.forEach(async (image) => {
+      if (image.startsWith("banner")) {
+        const extension = image.split(".").pop();
+        await fs.promises.copyFile(
+          `${studyDir}/images/${image}`,
+          `${distDir}/images/${studyKey}.${extension}`
+        );
+      } else {
+        await fs.promises.copyFile(
+          `${studyDir}/images/${image}`,
+          `${distDir}/images/${studyKey}.${image}`
+        );
+      }
+    });
+
+    // process surveys
+    try {
+      const surveys = await fs.promises.readdir(`${studyDir}/surveys`);
+      surveys.forEach(async (survey) => {
+        processSurvey(`${studyDir}/surveys/${survey}`);
+      });
+    } catch (err) {
+      // absorb the error if it's because the study has no study specific surveys
+      if (err.code !== "ENOENT" || err.syscall !== "scandir") {
+        throw err;
+      }
+    }
+  });
+}
+
+async function processSurvey(surveyPath) {
+  const relativePath = path.relative(process.cwd(), surveyPath);
+  const file = fs.promises.readFile(surveyPath, { encoding: "utf-8" });
+
+  const fileName = "blah";
+
+  const hash = crypto.createHash("md5").update(file).digest("hex");
+  const cfg = YAML.parse(file);
+
+  /*
+  // Validate the survey
+  if (fileName !== cfg.key) {
+    throw new Error(`Key and Filename mismatch: (survey) ${file}"`);
+  }
+
+  const schemaErrors = validateSchema("survey", data);
+  if (schemaErrors.length) {
+    console.error(schemaErrors);
+    throw new Error(`Schema validation failed for "${name}"`);
+  }
+  */
+  //
+
+  //
+  //
+
+  //
   //
 }
 
+async function build() {
+  await Promise.all([
+    mkdir(`${__dirname}/dist/images`),
+    mkdir(`${__dirname}/dist/studies`),
+    mkdir(`${__dirname}/dist/surveys`),
+  ]);
+  processStudies();
+}
+build();
 /*
 
 const surveyCfgMap = {};
