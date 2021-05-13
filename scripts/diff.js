4095db60a57e68ce51f89a281b4201f048d97f98
Kyle Landell <kyle.landell@mssm.edu>
2021-05-13 16:00:38 -0400

    Add multimedia and a v2 buildscript

--- scripts/diff.js
+++ scripts/diff.js
@@ -6,12 +6,15 @@ const chalk = require("chalk");
 
 // TODO: Use lowercase, dash no underscore
 // TODO: Only allow override of library surveys
+// TODO: Not schema validating surveys
+// TODO: Add schema validation for multimedia
 
 const log = {
   error: (t) => console.log(chalk.red(t)),
   warning: (t) => console.log(chalk.yellow(t)),
   info: (t) => console.log(chalk.cyan(t)),
   important: (t) => console.log(chalk.green(t)),
+  plain: (t) => console.log(t),
 };
 
 const validateSchema = require("./validate_schema");
@@ -24,18 +27,22 @@ const nextVersions = {
   active: {
     studies: {},
     surveys: {},
+    multimedia: {},
   },
   inactive: {
     studies: {},
     surveys: {},
+    multimedia: {},
   },
 };
 
 const surveyLibrary = new Set();
+const multimediaLibrary = new Set();
 
 const diffs = new Map();
 diffs.set("studies", new Set());
 diffs.set("surveys", new Set());
+diffs.set("multimedia", new Set());
 
 async function getFile(path) {
   const data = await fs.promises.readFile(path, { encoding: "utf-8" });
@@ -46,12 +53,11 @@ async function getFile(path) {
 }
 
 async function processSurveys() {
-  log.info("*****************************************");
-  log.info("****         Diffing Surveys         ****");
-  log.info("*****************************************");
+  log.plain("*****************************************");
+  log.plain("****         Diffing Surveys         ****");
+  log.plain("*****************************************");
 
   const dir = path.join(__dirname, "../cfg/surveys");
-
   const surveyGroups = await fs.promises.readdir(dir);
 
   const promises = surveyGroups.map(async (surveyGroup) => {
@@ -133,16 +139,103 @@ async function processSurveys() {
       // write to the versions file
       nextVersions.active.surveys[name] = [nextHash, nextVersion];
     });
-
     return Promise.all(surveyPromises);
   });
   return Promise.all(promises);
 }
 
+async function processMultimedia() {
+  log.plain("*****************************************");
+  log.plain("****       Diffing Multimedia        ****");
+  log.plain("*****************************************");
+
+  const dir = path.join(__dirname, "../cfg/multimedia");
+  const multimediaGroups = await fs.promises.readdir(dir);
+
+  const promises = multimediaGroups.map(async (multimediaGroup) => {
+    const groupDir = `${dir}/${multimediaGroup}`;
+    const multimedia = await fs.promises.readdir(groupDir);
+    const multimediaPrefix = multimediaGroup;
+
+    const multimediaPromises = multimedia.map(async (file) => {
+      const rootName = file.replace(/\.yaml$|\.yml$/, "");
+      const extension = file.split(".").pop();
+      const name = `${multimediaPrefix}::${rootName}`;
+
+      const { data, hash: nextHash } = await getFile(`${groupDir}/${file}`);
+      const [lastHash, lastVersion] = versions.active?.multimedia[name] ||
+        versions.inactive?.multimedia[name] || [null, null];
+      let nextVersion = lastVersion;
+
+      log.info(`[${name}] Processing`);
+
+      // skip if inactive
+      if (!data.active) {
+        log.warning(`[${name}] Inactive.`);
+        // Write out as inactive. If it's new and inactive, create a new record.
+        // If it's changed an inactive, we don't care. The changes will be picked
+        // up when/if it's moved to active again.
+        nextVersions.inactive.multimedia[name] = [
+          lastHash || nextHash,
+          lastVersion || 1,
+        ];
+        return;
+      }
+
+      if (rootName !== data.key) {
+        log.error(`[${name}] Multimedia key and Filename mismatch.`);
+        return;
+      }
+
+      // If we are using .yml, automatically move to .yaml.
+      if (extension === "yml") {
+        try {
+          // see if the .yaml file that we want to move it to already exists
+          await fs.promises.stat(`${groupDir}/${rootName}.yaml`);
+          log.error(
+            `[${name}] Uses a .yml extension and another file with a .yaml extension already exists. Could not automatically rename.`
+          );
+          return;
+        } catch (err) {
+          log.warning(`[${name}] Renaming to ${name}.yaml.`);
+          // if we get here it means we can rename
+          await fs.promises.rename(
+            `${groupDir}/${rootName}.yml`,
+            `${groupDir}/${rootName}.yaml`
+          );
+        }
+      }
+
+      // add this multimedia to the multimedia library
+      multimediaLibrary.add(name);
+
+      if (!lastHash || !lastVersion) {
+        nextVersion = 1;
+        // If this is the first time seeing this file set the version to the
+        // existing version in the yaml, or 1.
+        log.info(`[${name}] New Multimedia. Setting version to 1.`);
+        diffs.get("multimedia").add(name);
+      } else if (lastHash !== nextHash) {
+        nextVersion = lastVersion + 1;
+        // If we've seen this file, but it has changed, increment the version
+        log.info(
+          `[${name}] Multimedia modified. Bumping version to ${nextVersion}.`
+        );
+        diffs.get("multimedia").add(name);
+      }
+      log.important(`[${name}] Finished processing. Writing to version.lock.`);
+      // write to the versions file
+      nextVersions.active.multimedia[name] = [nextHash, nextVersion];
+    });
+    return Promise.all(multimediaPromises);
+  });
+  return Promise.all(promises);
+}
+
 async function processStudies() {
-  log.info("*****************************************");
-  log.info("****         Diffing Studies         ****");
-  log.info("*****************************************");
+  log.plain("*****************************************");
+  log.plain("****         Diffing Studies         ****");
+  log.plain("*****************************************");
 
   const dir = path.join(__dirname, "../cfg/studies");
   const files = await fs.promises.readdir(dir);
@@ -204,14 +297,27 @@ async function processStudies() {
     }
 
     // warn if requested survey is not available
-    data.surveys.forEach((s) => {
+    data.surveys?.forEach((s) => {
       if (
         !surveyLibrary.has(`${name}::${s}`) &&
         !surveyLibrary.has(`library::${s}`) &&
         !surveyLibrary.has(s)
       ) {
         log.warning(
-          `[${name}] Includes Survey "${s}" which does not exist in the survey library.`
+          `[${name}] Includes survey "${s}" which does not exist in the survey library.`
+        );
+      }
+    });
+
+    // warn if requested multimedia is not available
+    data.multimedia?.forEach((m) => {
+      if (
+        !multimediaLibrary.has(`${name}::${m}`) &&
+        !multimediaLibrary.has(`library::${m}`) &&
+        !multimediaLibrary.has(m)
+      ) {
+        log.warning(
+          `[${name}] Includes multimedia "${m}" which does not exist in the multimedia library.`
         );
       }
     });
@@ -229,7 +335,7 @@ async function processStudies() {
       let surveyModified = false;
       // If it is a study, and we didn't change the file itself,
       // check to see if any of the study's surveys have changed.
-      data.surveys.forEach((s) => {
+      data.surveys?.forEach((s) => {
         if (
           (surveyLibrary.has(`${name}::${s}`) &&
             diffs.get("surveys").has(`${name}::${s}`)) ||
@@ -244,7 +350,25 @@ async function processStudies() {
         }
       });
 
-      if (surveyModified) {
+      let multimediaModified = false;
+      // If it is a study, and we didn't change the file itself,
+      // check to see if any of the study's multimedia tasks have changed.
+      data.multimedia?.forEach((m) => {
+        if (
+          (multimediaLibrary.has(`${name}::${m}`) &&
+            diffs.get("multimedia").has(`${name}::${m}`)) ||
+          (!multimediaLibrary.has(`${name}::${m}`) &&
+            multimediaLibrary.has(m) &&
+            diffs.get("multimedia").has(m))
+        ) {
+          log.info(
+            `[${name}] Contains modified multimedia ${m}. The study version will be bumped.`
+          );
+          multimediaModified = true;
+        }
+      });
+
+      if (surveyModified || multimediaModified) {
         nextVersion = lastVersion + 1;
         // If we've seen this file, but it has changed, increment the version
         log.info(`[${name}] Study bumped to version ${nextVersion}.`);
@@ -261,6 +385,7 @@ async function processStudies() {
 
 async function diff() {
   await processSurveys();
+  await processMultimedia();
   await processStudies();
   // write out the new versions file
   await fs.promises.writeFile(
