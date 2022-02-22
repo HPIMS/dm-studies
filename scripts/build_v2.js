be5615b966ca5396e4956059bb4ee47e483ee240
Kyle Landell <kyle.landell@mssm.edu>
2022-02-22 12:10:03 -0500

    v3 tweaks

--- scripts/build_v2.js
+++ scripts/build_v2.js
@@ -299,10 +299,12 @@ async function processStudies() {
     delete data.visibility;
     delete data.irb;
 
+    delete data.pi;
     delete data.studyTeamId;
     delete data.openEnrollment;
     delete data.shortDescription;
     delete data.longDescription;
+    delete data.notes;
     delete data.eligibilityCriteria;
     delete data.timeResponsibility;
     delete data.imageId;
